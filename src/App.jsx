d7603cce675fbc46dfa5d58bb95a2fa2336ecb40
JAYAPRAKASH <153973163+karunajaya@users.noreply.github.com>
2024-10-05 20:57:45 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from './Nav';
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <Nav></Nav>
+      </div>
+    </>
+  )
+}
+
+export default App
